e7b532bdddf4ecc0bc754c55b57098110e02c970
Miro Kakkonen <miro.kakkonen@protonmail.com>
2026-02-09 22:56:26 +0200

    Add product.active check to testgen

--- pricetracker/models.py
+++ pricetracker/models.py
@@ -57,30 +57,34 @@ def init_db_command():
 def generate_test_data():
     import datetime
     import random
+
     u = User(
         email="test-user-1@localhost",
         password=hashlib.sha256("password".encode()).digest()
     )
+    db.session.add(u)
+
+    # fun bug: the default value of "True" for the "active" parameter is only set when the product is actually inserted into the database, so we have to explicitly set it here if we want to test for product.active in the code 
     products = [
-        Product(name="PS5", url="https://www.gigantti.fi/product/gaming/pelikonsolit-ja-tarvikkeet/playstation/playstation-konsolit/playstation-5-slim-standard-edition-e-runko-1-tb/988057iuoe", notes="Standard edition"),
-        Product(name="Ikea desk", url="https://www.ikea.com/fi/fi/p/anfallare-alex-tyoepoeytae-bambu-mustanruskea-s89417745/?recently_viewed=b"),
+        Product(name="PS5", url="https://www.gigantti.fi/product/gaming/pelikonsolit-ja-tarvikkeet/playstation/playstation-konsolit/playstation-5-slim-standard-edition-e-runko-1-tb/988057iuoe", notes="Standard edition", active=True),
+        Product(name="Ikea desk", url="https://www.ikea.com/fi/fi/p/anfallare-alex-tyoepoeytae-bambu-mustanruskea-s89417745/?recently_viewed=b", active=True),
         Product(name="Broken product", url="https://www.outdated_url.com", notes="This is a product with a non-functional url.", active=False),
     ]
+
     for product in products:
-        count = 100
-        interval = datetime.timedelta(days=1)
-        now = datetime.datetime.now() - count * interval
-        for i in range(count):
-            price= Price(
-                value=round(random.random() * 100, 2),
-                timestamp=now,
-            )
-            now += interval
-            product.prices.append(price)
-
         u.products.append(product)
+        if product.active:
+            count = 100
+            interval = datetime.timedelta(days=1)
+            now = datetime.datetime.now() - count * interval
+            for i in range(count):
+                price= Price(
+                    value=round(random.random() * 100, 2),
+                    timestamp=now,
+                )
+                now += interval
+                product.prices.append(price)
 
-    db.session.add(u)
     try:
         db.session.commit()
     except exc.IntegrityError:
