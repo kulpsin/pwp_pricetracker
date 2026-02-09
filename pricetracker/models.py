6f8d21fce31276c557b68c58f2f409146dcb5f7a
Olli Puhakka <opuhakka@student.oulu.fi>
2026-02-09 20:15:44 +0200

    Renamed database columns - product.product -> product.name - price.price -> price.value

--- pricetracker/models.py
+++ pricetracker/models.py
@@ -18,7 +18,7 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"))
-    product = db.Column(db.String(128), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     url = db.Column(db.String(512), nullable=False)
     notes = db.Column(db.String(512), nullable=True)
     active = db.Column(db.Boolean, nullable=False, default=True)
@@ -30,7 +30,10 @@ class Product(db.Model):
 class Price(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey("product.id", ondelete="CASCADE"))
-    price = db.Column(db.Float, nullable=False)
+    value = db.Column(db.Float, nullable=False)
+    #TODO: support for different currencies? EUR,USD,...
+    #      The currency could be converted on the fly and
+    #      the value would be always stored as EUR?
     timestamp = db.Column(db.DateTime, nullable=False)
 
     product = db.relationship("Product", back_populates="prices")
@@ -59,9 +62,9 @@ def generate_test_data():
         password=hashlib.sha256("password".encode()).digest()
     )
     products = [
-        Product(product="PS5", url="https://www.gigantti.fi/product/gaming/pelikonsolit-ja-tarvikkeet/playstation/playstation-konsolit/playstation-5-slim-standard-edition-e-runko-1-tb/988057iuoe", notes="Standard edition"),
-        Product(product="Ikea desk", url="https://www.ikea.com/fi/fi/p/anfallare-alex-tyoepoeytae-bambu-mustanruskea-s89417745/?recently_viewed=b"),
-        Product(product="Broken product", url="https://www.outdated_url.com", notes="This is a product with a non-functional url.", active=False),
+        Product(name="PS5", url="https://www.gigantti.fi/product/gaming/pelikonsolit-ja-tarvikkeet/playstation/playstation-konsolit/playstation-5-slim-standard-edition-e-runko-1-tb/988057iuoe", notes="Standard edition"),
+        Product(name="Ikea desk", url="https://www.ikea.com/fi/fi/p/anfallare-alex-tyoepoeytae-bambu-mustanruskea-s89417745/?recently_viewed=b"),
+        Product(name="Broken product", url="https://www.outdated_url.com", notes="This is a product with a non-functional url.", active=False),
     ]
     for product in products:
         count = 100
@@ -69,7 +72,7 @@ def generate_test_data():
         now = datetime.datetime.now() - count * interval
         for i in range(count):
             price= Price(
-                price=round(random.random() * 100, 2),
+                value=round(random.random() * 100, 2),
                 timestamp=now,
             )
             now += interval
