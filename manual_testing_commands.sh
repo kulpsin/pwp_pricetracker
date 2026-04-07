fa1207e742cf561e872991c58e41b06a3f1cfda9
Miro Kakkonen <miro.kakkonen@protonmail.com>
2026-04-07 20:00:10 +0300

    Update manual testing commands for deployment

--- manual_testing_commands.sh
+++ manual_testing_commands.sh
@@ -1,31 +1,31 @@
 API_KEY = 
 
-curl -i -X POST http://127.0.0.1:5000/api/users/ \
+curl -i -X POST http://20.203.243.165/api/users/ \
   -H "Content-Type: application/json" \
-  -d '{"email": "miro@e.mail"}'
+  -d '{"email": "user@e.mail"}'
 
-curl -X PUT http://127.0.0.1:5000/api/users/USER_UUID/ \
+curl -X PUT http://20.203.243.165/api/users/USER_UUID/ \
   -H "Content-Type: application/json" \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ" \
+  -H "X-Api-Key: APIKEYHERE" \
   -d '{"email": "newemail@example.com"}'
 
-curl -X DELETE http://127.0.0.1:5000/api/users/USER_UUID/ \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ"
+curl -X DELETE http://20.203.243.165/api/users/USER_UUID/ \
+  -H "X-Api-Key: APIKEYHERE"
 
-curl http://127.0.0.1:5000/api/products/
+curl http://20.203.243.165/api/products/
 
-curl http://127.0.0.1:5000/api/products/PRODUCT_HRUID/
+curl http://20.203.243.165/api/products/PRODUCT_HRUID/
 
-curl -X POST http://127.0.0.1:5000/api/products/ \
+curl -X POST http://20.203.243.165/api/products/ \
   -H "Content-Type: application/json" \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ" \
+  -H "X-Api-Key: APIKEYHERE" \
   -d '{
     "name": "Car 9000",
     "url": "https://example.com/product",
     "active": true
   }'
 
-  curl -X POST http://127.0.0.1:5000/api/products/ \
+  curl -X POST http://20.203.243.165/api/products/ \
   -H "Content-Type: application/json" \
   -d '{
     "name": "Car 9000",
@@ -33,26 +33,26 @@ curl -X POST http://127.0.0.1:5000/api/products/ \
     "active": true
   }'
 
-curl -X PUT http://127.0.0.1:5000/api/products/PRODUCT_HRUID/ \
+curl -X PUT http://20.203.243.165/api/products/PRODUCT_HRUID/ \
   -H "Content-Type: application/json" \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ" \
+  -H "X-Api-Key: APIKEYHERE" \
   -d '{
     "name": "Updated Product Name",
     "url": "https://example.com/product-updated"
   }'
 
-curl -X DELETE http://127.0.0.1:5000/api/products/car-9000-1/ \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ"
+curl -X DELETE http://20.203.243.165/api/products/car-9000-1/ \
+  -H "X-Api-Key: APIKEYHERE"
 
-curl http://127.0.0.1:5000/api/products/PRODUCT_HRUID/prices/
+curl http://20.203.243.165/api/products/PRODUCT_HRUID/prices/
 
-curl -X POST http://127.0.0.1:5000/api/products/PRODUCT_HRUID/prices/ \
+curl -X POST http://20.203.243.165/api/products/PRODUCT_HRUID/prices/ \
   -H "Content-Type: application/json" \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ" \
+  -H "X-Api-Key: APIKEYHERE" \
   -d '{
     "value": 99.99,
     "timestamp": "2026-01-15T12:00:00"
   }'
 
-curl -X DELETE http://127.0.0.1:5000/api/products/PRODUCT_HRUID/prices/PRICE_ID/ \
-  -H "X-Api-Key: L7B30VROoni2w15LrG_OGuW0VjzaCUtn3XP_82BHiwQ"
+curl -X DELETE http://20.203.243.165/api/products/PRODUCT_HRUID/prices/PRICE_ID/ \
+  -H "X-Api-Key: APIKEYHERE"
